a859319492bcd30add8eb8e9153b8ebc9f3ac11f
Ahmed-Ali-0 <71400128+Ahmed-Ali-0@users.noreply.github.com>
2020-10-02 14:38:33 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+// Created by: Ahmed Ali
+// Created date : Oct2, 2020
+// 
+// This program use sonar and neoplixels
+let distance_to_sonar = sonar.ping(
+DigitalPin.P1,
+DigitalPin.P2,
+PingUnit.Centimeters
+)
+basic.showNumber(distance_to_sonar)
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.clear()
+strip.show()
+basic.showIcon(IconNames.Yes)
+basic.forever(function () {
+    distance_to_sonar = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    if (distance_to_sonar > 4) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (false) {
+        if (true) {
+        	
+        } else if (false) {
+        	
+        } else {
+        	
+        }
+    } else if (false) {
+    	
+    }
+    strip.show()
+    basic.showNumber(distance_to_sonar)
+    basic.pause(200)
+})
